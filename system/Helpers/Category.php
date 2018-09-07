3869a8e1a735d570a17aa6f4ae815a54251f4c2b
MPE <info@ciaompe.com>
2018-09-07 18:32:38 +0530

    Update Category.php

--- system/Helpers/Category.php
+++ system/Helpers/Category.php
@@ -2,28 +2,21 @@
 
 namespace Ciaompe\Helpers;
 
-use Ciaompe\Helpers\Database;
-
 class Category {
 
-    protected $db;
-
-    public function __construct(Database $db) {
-        $this->db = $db;
-    }
 	/*
 	Build category recursive method
 	*/
 	public static function buildTree(array $elements, $parentId = 0) {
 	    $branch = array();
 	    foreach ($elements as $element) {
-	        if ($element['parent'] == $parentId) {
-	            $children = self::buildTree($elements, $element['id']);
-	            if ($children) {
-	                $element['children'] = $children;
-	            }
-	            $branch[] = $element;
-	        }
+		if ($element['parent'] == $parentId) {
+		    $children = self::buildTree($elements, $element['id']);
+		    if ($children) {
+			$element['children'] = $children;
+		    }
+		    $branch[] = $element;
+		}
 	    }
 	    return $branch;
 	}
